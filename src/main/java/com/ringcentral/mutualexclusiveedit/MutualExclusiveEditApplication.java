612cdcd0c44e7ae8ffa3e34f6c92c12ba3994b8c
felixChen464 <34159016+felixChen464@users.noreply.github.com>
2020-07-11 20:35:34 +0800

    Add files via upload
    init

---
+++ src/main/java/com/ringcentral/mutualexclusiveedit/MutualExclusiveEditApplication.java
@@ -0,0 +1,13 @@
+package com.ringcentral.mutualexclusiveedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualExclusiveEditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualExclusiveEditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ringcentral/mutualexclusiveedit/MutualExclusiveEditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ringcentral.mutualexclusiveedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualExclusiveEditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
