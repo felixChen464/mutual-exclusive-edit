2bc77fb0a3f2677890265fe57bccaa2da53d15a0
felix <385681914@qq.com>
2020-07-16 23:45:21 +0800

    1、前端代码首次提交

---
+++ src/main/resources/static/api.js
@@ -0,0 +1,14 @@
+/**
+ * 后端api
+ */
+
+const common_file_record_pattern = '/api/file/record';
+const common_file_access_pattern = '/api/file/';
+
+
+
+
+
+function handleError(result) {
+    console.error(result.message)
+}
